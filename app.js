90b6bcc5e5ec1248ce1dbe3ed2e346f79566e2cc
prerabale <wlw.pr@foxmail.com>
2015-07-17 17:58:22 +0800

    完成detail页面的预编写

--- app.js
+++ app.js
@@ -59,14 +59,33 @@ app.get( '/', function( req, res ){
     }]
   });
 });
-app.get( '/detail/:id', function( req, res ){
+app.get( '/movie/:id', function( req, res ){
   res.render( 'detail', {
-    title : 'Movie 详情页'
+    title : 'Movie 详情页',
+    movie : {
+      doctor: '何塞·帕迪利亚',
+      country: '美国',
+      title: '机械战警',
+      year: 2014,
+      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+      language: '英语',
+      flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+      summary: '翻拍自1987年同名科幻经典，由《精英部队》导演何塞·帕迪利亚指执导的新版《机械战警》发布同款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。预告片末更亮出了本片将登陆IMAX巨幕。新版《机械战警》故事背景跟原版一样，依旧设定在工业城市底特律，但故事年代已由之前设定的2020年变为2028年，并且故事格局也明显扩大。在片中，金纳曼饰演的好警察墨菲将会被歹徒“杀死”，然后被进行军火开发的机器人公司Omni Corp改造成半人半机器的“机械战警”。'
+    }
   });
 });
-app.get( '/admin/movie', function( req, res ){
+app.get( '/movie/admin', function( req, res ){
   res.render( 'admin', {
-    title : 'Movie 后台录入页'
+    title : 'Movie 后台录入页',
+    movie : {
+      title :'',
+      doctor : '',
+      country : '',
+      language : '',
+      poster : '',
+      year : '',
+      summary : ''
+    }
   });
 });
 app.get( '/list/:id', function( req, res ){
