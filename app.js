8b50ad8a2582a991acaa2fd3f8ff38a9a20d6696
prerabale <wlw.pr@foxmail.com>
2015-07-17 16:28:54 +0800

    1.完成组件的安装和文件的布局 2.首页功能实现

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views/pages' );
 app.set('view engine', 'jade');
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, './bower_components')));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +22,58 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+/*
+app.get( '/', function( req, res ){
+  res.render( 'index', {
+    title : 'Movie 首页'
+  });
+});
+*/
+app.get( '/', function( req, res ){
+  res.render('index',{
+    title: 'Movie 首页',
+    movies: [{
+    title: '机械战警',
+    _id: 1,
+    poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    },{
+    title: '机械战警',
+    _id: 2,
+    poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    },{
+    title: '机械战警',
+    _id: 3,
+    poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    },{
+    title: '机械战警',
+    _id: 4,
+    poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    },{
+    title: '机械战警',
+    _id: 5,
+    poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    },{
+    title: '机械战警',
+    _id: 6,
+    poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    }]
+  });
+});
+app.get( '/detail/:id', function( req, res ){
+  res.render( 'detail', {
+    title : 'Movie 详情页'
+  });
+});
+app.get( '/admin/movie', function( req, res ){
+  res.render( 'admin', {
+    title : 'Movie 后台录入页'
+  });
+});
+app.get( '/list/:id', function( req, res ){
+  res.render( 'list', {
+    title : 'Movie 列表页'
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
