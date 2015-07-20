5f971c87a16670a11d64e9bdcf85a965d572d337
prerabale <wlw.pr@foxmail.com>
2015-07-20 16:23:58 +0800

    list.jade 中 item 遍历的是movies，app.js中/admin/list 里的数组改为movies

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 // view engine setup
 app.set('views', './views/pages' );
 app.set('view engine', 'jade');
-app.use(bodyParser());
+//app.use(bodyParser());
 app.use(express.static(path.join(__dirname, './bower_components')));
 
 // uncomment after placing your favicon in /public
@@ -74,7 +74,7 @@ app.get( '/movie/:id', function( req, res ){
     }
   });
 });
-app.get( '/movie/admin', function( req, res ){
+app.get( '/admin/movie', function( req, res ){
   res.render( 'admin', {
     title : 'Movie 后台录入页',
     movie : {
@@ -82,15 +82,28 @@ app.get( '/movie/admin', function( req, res ){
       doctor : '',
       country : '',
       language : '',
-      poster : '',
       year : '',
-      summary : ''
+      summary : '',
+      poster : '',
+      flash : '',
     }
   });
 });
-app.get( '/list/:id', function( req, res ){
+app.get( '/admin/list', function( req, res ){
+  console.log( 'comming list' );
   res.render( 'list', {
-    title : 'Movie 列表页'
+    title : 'Movie 列表页',
+    movies : [{
+      title : '机械战警',
+      _id : 1,
+      doctor : '何塞·帕迪里亚',
+      country : '美国',
+      year : 2014,
+      language : '英语',
+      flash : 'http://player.youku.com/player.php/sid/XNJA1Njc0NTUy/v.swf',
+      summary:'翻拍自1987年同名科幻经典，由《精英部队》导演何塞·帕迪利亚指执导的新版《机械战警》发布同款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。预告片末更亮出了本片将登陆IMAX巨幕。新版《机械战警》故事背景跟原版一样，依旧设定在工业城市底特律，但故事年代已由之前设定的2020年变为2028年，并且故事格局也明显扩大。在片中，金纳曼饰演的好警察墨菲将会被歹徒“杀死”，然后被进行军火开发的机器人公司Omni Corp改造成半人半机器的“机械战警”。'
+
+    }]
   });
 });
 
@@ -119,10 +132,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  console.log('message:', err.message );
 });
 
 
